983cf608aa06afa44d8fa6d7b7e2cbade814b7c1
chandankrishnan <chandan.mandre10@gmail.com>
2016-10-24 10:49:18 +0530

    readfile

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,24 @@
  */
 var express = require('express'),
     router = express.Router(),
+    fs = require('fs'),
     convert = require('../model/convert.js');
+
+router.get('/get/csv/file', function (req, res) {
+    console.log('inside get')
+    fs.readdir('./File', (err, files) => {
+        console.log(files)
+        for (var i = 0; i < files.length; i++) {
+            var file = './File/' + files[i];
+            console.log("Start: " + file);
+            fs.stat(file, function (err, stats) {
+                console.log(file);
+                console.log(stats["size"]);
+            });
+        }
+    })
+
+})
 /**
  * it convert into json when we pass .csv file contain URL
  * @param      {String} --q(url)
@@ -25,4 +42,5 @@ router.get("/csv/to/json", function (req, res) {
     }
 })
 
-module.exports = router;    
+module.exports = router;
+
